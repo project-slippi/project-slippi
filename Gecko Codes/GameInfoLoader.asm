d4b1161036bd561f27219ddff5485d420a28cab2
Jas Laferriere <jas.laferriere@gmail.com>
2017-08-01 22:24:28 -0700

    Updated codes

---
+++ Gecko Codes/GameInfoLoader.asm
@@ -0,0 +1,125 @@
+################################################################################
+#                      Inject at address 8016e7bc
+# Function is StartMelee and we are loading game information right before
+# it gets read to initialize the match
+################################################################################
+
+#replaced code line is executed at the end
+
+################################################################################
+#                   subroutine: gameInfoLoad
+# description: reads game info from slippi and loads those into memory
+# addresses that will be used
+################################################################################
+#create stack frame and store link register
+mflr r0
+stw r0, 0x4(r1)
+stwu r1,-0x20(r1)
+
+# initialize transfer with slippi device
+bl startExiTransfer
+
+# request game information from slippi
+li r3,0x75
+bl sendByteExi
+
+bl readWordExi #stageId
+sth r3,0xE(r31)
+bl readWordExi #player 1 info
+stw r3,0x60(r31)
+bl readWordExi #player 2 info
+stw r3,0x84(r31)
+bl readWordExi #player 3 info
+stw r3,0xA8(r31)
+bl readWordExi #player 4 info
+stw r3,0xCC(r31)
+
+# write constant game info
+li r3,0x20 #match type (stocks)
+stb r3,0x0(r31)
+
+bl endExiTransfer
+
+CLEANUP:
+#restore registers and sp
+lwz r0, 0x24(r1)
+addi r1, r1, 0x20
+mtlr r0
+
+b GECKO_END
+
+################################################################################
+#                  subroutine: startExiTransfer
+#  description: prepares port B exi to be written to
+################################################################################
+startExiTransfer:
+lis r11, 0xCC00 #top bytes of address of EXI registers
+
+#disable read/write protection on memory pages
+lhz r10, 0x4010(r11)
+ori r10, r10, 0xFF
+sth r10, 0x4010(r11) # disable MP3 memory protection
+
+#set up EXI
+li r10, 0xB0 #bit pattern to set clock to 8 MHz and enable CS for device 0
+stw r10, 0x6814(r11) #start transfer, write to parameter register
+
+blr
+
+################################################################################
+#                    subroutine: sendByteExi
+#  description: sends one byte over port B exi
+#  inputs: r3 byte to send
+################################################################################
+sendByteExi:
+lis r11, 0xCC00 #top bytes of address of EXI registers
+li r10, 0x5 #bit pattern to write to control register to write one byte
+
+#write value in r3 to EXI
+slwi r3, r3, 24 #the byte to send has to be left shifted
+stw r3, 0x6824(r11) #store current byte into transfer register
+stw r10, 0x6820(r11) #write to control register to begin transfer
+
+#wait until byte has been transferred
+EXI_CHECK_RECEIVE_WAIT:
+lwz r10, 0x6820(r11)
+andi. r10, r10, 1
+bne EXI_CHECK_RECEIVE_WAIT
+
+blr
+
+################################################################################
+#                    subroutine: readWordExi
+#  description: reads one word over port B exi
+#  outputs: r3 received word
+################################################################################
+readWordExi:
+lis r11, 0xCC00 #top bytes of address of EXI registers
+li r10, 0x31 #bit pattern to write to control register to read four bytes
+stw r10, 0x6820(r11) #write to control register to begin transfer
+
+#wait until byte has been transferred
+EXI_CHECK_RECEIVE_WAIT_READWORD:
+lwz r10, 0x6820(r11)
+andi. r10, r10, 1
+bne EXI_CHECK_RECEIVE_WAIT_READWORD
+
+#read values from transfer register to r3 for output
+lwz r3, 0x6824(r11) #read from transfer register
+
+blr
+
+################################################################################
+#                  subroutine: endExiTransfer
+#  description: stops port B writes
+################################################################################
+endExiTransfer:
+lis r11, 0xCC00 #top bytes of address of EXI registers
+
+li r10, 0
+stw r10, 0x6814(r11) #write 0 to the parameter register
+
+blr
+
+GECKO_END:
+mr r3, r31 #execute replaced code line
--- Gecko Codes/Slippi.asm
+++ Gecko Codes/Slippi.asm
@@ -1,9 +1,10 @@
 ################################################################################
-#                      Inject at address 8038059c
-#
+#                      Inject at address 8006b0dc
+# Function is PlayerThink_ControllerInputsToDataOffset. Injection location
+# suggested by Achilles
 ################################################################################
 
-#replaced code line does not need to be executed
+#replaced code line is executed at the end
 
 ################################################################################
 #                   subroutine: readInputs
@@ -14,14 +15,16 @@
 mflr r0
 stw r0, 0x4(r1)
 stwu r1,-0x20(r1)
+stw r31,0x1C(r1)
 stw r3,0x18(r1)
 stw r4,0x14(r1)
 
+# initalize
+lwz r3,0x10(r27) #get player struct
+lwz r31,0x2c(r3) #get pointed player block
+
 bl startExiTransfer
 
-# this is currently pretty stupid but we are gonna request a frame of
-# inputs for a player that certainly doesn't exist to only get the random
-# seed back. Could be more efficient
 li r3,0x76
 bl sendByteExi #init slip
 
@@ -29,28 +32,41 @@ bl sendByteExi #init slip
 lis r4,0x8048
 lwz r4,-0x62A8(r4) # load scene controller frame count
 lis r3,0x8047
-lwz r3,-0x493C(r3) # load match frame count
+lwz r3,-0x493C(r3) #load match frame count
 cmpwi r3, 0
-bne SKIP_FRAME_COUNT_ADJUST
+bne SKIP_FRAME_COUNT_ADJUST #this makes it so that if the timer hasn't started yet, we have a unique frame count still
 sub r3,r3,r4
 li r4,-0x7B
 sub r3,r4,r3
 SKIP_FRAME_COUNT_ADJUST:
 bl sendWordExi
 
-li r3,0xA #player slot that doesn't exist
+lbz r3,0xC(r31)			#player slot
 bl sendByteExi
 
 REPLAY:
 bl readWordExi
 lis r4,0x804D
 stw r3,0x5F90(r4) #RNG seed
+bl readWordExi
+stw r3,0x620(r31) #analog X
+bl readWordExi
+stw r3,0x624(r31) #analog Y
+bl readWordExi
+stw r3,0x638(r31) #cstick X
+bl readWordExi
+stw r3,0x63C(r31) #cstick Y
+bl readWordExi
+stw r3,0x650(r31) #trigger
+bl readWordExi
+stw r3,0x65C(r31) #buttons
 
 bl endExiTransfer
 
 CLEANUP:
 #restore registers and sp
 lwz r0, 0x24(r1)
+lwz r31, 0x1C(r1)
 lwz r3, 0x18(r1)
 lwz r4, 0x14(r1)
 addi r1, r1, 0x20
@@ -95,6 +111,28 @@ EXI_CHECK_RECEIVE_WAIT:
 lwz r10, 0x6820(r11)
 andi. r10, r10, 1
 bne EXI_CHECK_RECEIVE_WAIT
+
+blr
+
+################################################################################
+#                    subroutine: sendHalfExi
+#  description: sends two bytes over port B exi
+#  inputs: r3 bytes to send
+################################################################################
+sendHalfExi:
+lis r11, 0xCC00 #top bytes of address of EXI registers
+li r10, 0x15 #bit pattern to write to control register to write one byte
+
+#write value in r3 to EXI
+slwi r3, r3, 16 #the bytes to send have to be left shifted
+stw r3, 0x6824(r11) #store bytes into transfer register
+stw r10, 0x6820(r11) #write to control register to begin transfer
+
+#wait until byte has been transferred
+EXI_CHECK_RECEIVE_WAIT_HALF:
+lwz r10, 0x6820(r11)
+andi. r10, r10, 1
+bne EXI_CHECK_RECEIVE_WAIT_HALF
 
 blr
 
@@ -153,3 +191,4 @@ stw r10, 0x6814(r11) #write 0 to the parameter register
 blr
 
 GECKO_END:
+lbz r0, 0x2219(r31) #execute replaced code line
