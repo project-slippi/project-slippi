5048330ef9181b392a2e5ffab4407c669654237d
Jas Laferriere <jas.laferriere@gmail.com>
2017-08-03 23:19:52 -0700

    Adds some styled buttons

--- app/app/components/Home.js
+++ app/app/components/Home.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react'
+import { Button, Header, Icon, Grid } from 'semantic-ui-react'
 import styles from './Home.css';
 import { exec } from 'child_process';
 
@@ -21,13 +21,51 @@ export default class Home extends Component {
     });
   }
 
+  generateNav(iconName, header, subHeader, disabled) {
+    return (
+      <div>
+        <Button fluid={true} inverted={true} color={"green"} disabled={disabled}>
+          <Grid centered={true} columns={1}>
+            <Grid.Column>
+              <Header as='h2' inverted={true} color={"green"} textAlign={"center"}>
+                <Icon name={iconName} />
+                <Header.Content>
+                  {header}
+                  <Header.Subheader>
+                    {subHeader}
+                  </Header.Subheader>
+                </Header.Content>
+              </Header>
+            </Grid.Column>
+          </Grid>
+        </Button>
+      </div>
+    );
+  }
+
   render() {
+    let playFromFile = this.generateNav(
+      "disk outline",
+      "From File System",
+      "Play replays from files on your computer",
+      false
+    );
+
+    let streamFromSlippi = this.generateNav(
+      "microchip",
+      "Stream From Slippi Device",
+      "Stream replay from Slippi device",
+      true
+    );
+
     return (
       <div>
         <div className={styles.container} data-tid="container">
           <Button inverted={true} color={"green"} onClick={this.test}>
             Launch Replay
           </Button>
+          {playFromFile}
+          {streamFromSlippi}
         </div>
       </div>
     );
