68eba6fd9a100c039700d5e799fb9ac28ded7247
Jas Laferriere <jas.laferriere@gmail.com>
2017-08-03 22:15:39 -0700

    Adds simply changes for testing the launcher

--- app/app/components/Home.js
+++ app/app/components/Home.js
@@ -2,14 +2,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+import { exec } from 'child_process';
 
 export default class Home extends Component {
+  test() {
+    const dolphinPath = "D:\\Users\\Fizzi\\Documents\\Github\\Ishiiruka\\Binary\\x64";
+    const meleeFile = "C:\\Dolphin\\Games\\ssbm-v1_02.iso";
+    const command = `D: & cd \"${dolphinPath}\" & Dolphin.exe /b /e \"${meleeFile}\"`;
+    console.log(command);
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+  }
+
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <a onClick={this.test}>
+            <h2>Launch Replay</h2>
+          </a>
         </div>
       </div>
     );
