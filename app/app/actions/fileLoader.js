7fe3b8319d95291493abe57470bb91086bbcad59
jlaferri <jas.laferriere@gmail.com>
2017-09-18 22:15:56 -0700

    fork launcher logic based on platform

--- app/app/actions/fileLoader.js
+++ app/app/actions/fileLoader.js
@@ -36,6 +36,9 @@ export function playFile(file) {
       return;
     }
 
+    const platform = process.platform;
+    const isDev = process.env.NODE_ENV === "development";
+
     // For windows
     //const dolphinPath = "D:\\Users\\Fizzi\\Documents\\Github\\Ishiiruka\\Binary\\x64";
     //const meleeFile = "C:\\Dolphin\\Games\\ssbm-v1_02.iso";
@@ -45,21 +48,49 @@ export function playFile(file) {
     // 1) ISO Path
     // 2) Root Path
 
-    // For osx
-    const dolphinPath = "./app/dist/dolphin";
-    const appName = "Dolphin.app";
-    const meleeFile = "$HOME/Documents/Games/melee.iso";
-    const destinationFile = path.join(dolphinPath, 'Slippi', 'CurrentGame.slp');
-    const commands = [
-      `cp \"${filePath}\" \"${destinationFile}\"`,
-      `cd \"${dolphinPath}\"`,
-      `open \"${appName}\" --args -b -e \"${meleeFile}\"`
-    ];
-    const command = commands.join(' && ');
+    // Here we are going to build the platform-specific commands required to launch
+    // dolphin from the command line with the correct game
+    let commands, command, dolphinPath, destinationFile, meleeFile;
+    switch (platform) {
+    case "darwin": // osx
+      // When in development mode, use the build-specific dolphin version
+      // In production mode, only the build from the correct platform should exist
+      dolphinPath = isDev ? "./app/dolphin/macOS" : "./app/dolphin";
+      meleeFile = "$HOME/Documents/Games/melee.iso";
+      destinationFile = path.join(dolphinPath, 'Slippi', 'CurrentGame.slp');
 
-    //// First copy the selected file over to the Slippi folder of the playback dolphin
-    //const destinationFile = path.join(dolphinPath, 'Slippi', 'CurrentGame.slp');
-    //fs.copyFileSync(filePath, destinationFile);
+      // 1) Copy file to the playback dolphin build with the name CurrentGame.slp
+      // 2) Navigate to dolphin build path
+      // 3) Run dolphin with parameters to launch melee directly
+      commands = [
+        `cp \"${filePath}\" \"${destinationFile}\"`,
+        `cd \"${dolphinPath}\"`,
+        `open \"Dolphin.app\" --args -b -e \"${meleeFile}\"`
+      ];
+
+      // Join the commands with && which will execute the commands in sequence
+      command = commands.join(' && ');
+      break;
+    case "win32": // windows
+      // When in development mode, use the build-specific dolphin version
+      // In production mode, only the build from the correct platform should exist
+      dolphinPath = isDev ? "./app/dolphin/windows" : "./app/dolphin";
+      meleeFile = "C:\\Dolphin\\Games\\ssbm-v1_02.iso";
+      destinationFile = path.join(dolphinPath, 'Slippi', 'CurrentGame.slp');
+
+      // 1) Copy file to the playback dolphin build with the name CurrentGame.slp
+      // 2) Navigate to dolphin build path
+      // 3) Run dolphin with parameters to launch melee directly
+      commands = [
+        `copy \"${filePath}\" \"${destinationFile}\"`,
+        `cd \"${dolphinPath}\"`,
+        `Dolphin.exe /b /e \"${meleeFile}\"`
+      ];
+
+      // Join the commands with && which will execute the commands in sequence
+      command = commands.join(' & ');
+      break;
+    }
 
     exec(command, (error, stdout, stderr) => {
       // Apparently this callback happens before dolphin exits...
