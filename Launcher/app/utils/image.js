23c71330fba24a8e40452f47c25622d31736828d
Jas Laferriere <Fizzi36@gmail.com>
2017-11-24 21:34:34 -0800

    fix local images in dev

--- Launcher/app/utils/image.js
+++ Launcher/app/utils/image.js
@@ -8,8 +8,7 @@ export default function getLocalImage(image) {
   // This is the path of dolphin after this app has been packaged
   let imagePath = path.join(appPath, "../app.asar.unpacked/images");
   if (isDev) {
-    // Kind of ridiculous, there's probably a better way to do this
-    imagePath = path.join(appPath, "../../../../../../../app/images");
+    imagePath = "./images";
   }
 
   return path.join(imagePath, image);
