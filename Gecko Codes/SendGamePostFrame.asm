992ac5d0d6cdd55e529bad637e370e6bf6015cf4
Jas Laferriere <Fizzi36@gmail.com>
2018-06-28 23:53:46 -0700

    attempt to call game EXIImm function

--- Gecko Codes/SendGamePostFrame.asm
+++ Gecko Codes/SendGamePostFrame.asm
@@ -130,11 +130,11 @@ b GECKO_END
 #  description: prepares port B exi to be written to
 ################################################################################
 startExiTransfer:
-lis r11, 0xCC00 #top bytes of address of EXI registers
+//lis r11, 0xCC00 #top bytes of address of EXI registers
 
 #set up EXI
-li r10, 0xD0 #bit pattern to set clock to 8 MHz and enable CS for device 0
-stw r10, 0x6800(r11) #start transfer, write to parameter register
+//li r10, 0xB0 #bit pattern to set clock to 8 MHz and enable CS for device 0
+//stw r10, 0x6814(r11) #start transfer, write to parameter register
 
 blr
 
@@ -144,8 +144,11 @@ blr
 #  inputs: r3 byte to send
 ################################################################################
 sendByteExi:
-slwi r3, r3, 24 #the byte to send has to be left shifted
-li r4, 0x5 #bit pattern to write to control register to write one byte
+//slwi r3, r3, 24 #the byte to send has to be left shifted
+//li r4, 0x5 #bit pattern to write to control register to write one byte
+mr r4, r3
+li r3, 0
+li r5, 1
 b handleExi
 
 ################################################################################
@@ -154,8 +157,11 @@ b handleExi
 #  inputs: r3 bytes to send
 ################################################################################
 sendHalfExi:
-slwi r3, r3, 16 #the bytes to send have to be left shifted
-li r4, 0x15 #bit pattern to write to control register to write two bytes
+//slwi r3, r3, 16 #the bytes to send have to be left shifted
+//li r4, 0x15 #bit pattern to write to control register to write two bytes
+mr r4, r3
+li r3, 0
+li r5, 2
 b handleExi
 
 ################################################################################
@@ -164,7 +170,10 @@ b handleExi
 #  inputs: r3 word to send
 ################################################################################
 sendWordExi:
-li r4, 0x35 #bit pattern to write to control register to write four bytes
+//li r4, 0x35 #bit pattern to write to control register to write four bytes
+mr r4, r3
+li r3, 0
+li r5, 4
 b handleExi
 
 ################################################################################
@@ -177,38 +186,22 @@ b handleExi
 #  r3 value read from transfer register after operation
 ################################################################################
 handleExi:
-#write value in r3 to EXI
-stw r3, 0x6810(r11) #store data into transfer register
-b handleExiStart
+mflr r0
+stw r0, 0x4(r1)
+stwu r1,-0x20(r1)
 
-handleExiRetry:
-# this effectively calls endExiTransfer and then startExiTransfer again
-# the reason for this is on dolphin sometimes I would get an error that read:
-# Exception thrown at 0x0000000019E04D6B in Dolphin.exe: 0xC0000005: Access violation reading location 0x000000034BFF6820
-# this was causing data to not be written successfully and the only way I found
-# to not soft-lock the game (the receive_wait loop would loop forever) was
-# to do this
-li r10, 0
-stw r10, 0x6800(r11) #write 0 to the parameter register
-li r10, 0xD0 #bit pattern to set clock to 8 MHz and enable CS for device 0
-stw r10, 0x6800(r11) #start transfer, write to parameter register
+li r6, 1
 
-handleExiStart:
-stw r4, 0x680C(r11) #write to control register to begin transfer
+lis r7, 0x8034
+ori r7, r7, 0x5b64
+mtlr r7
+li r7, 0
+blrl
 
-li r9, 0
-#wait until byte has been transferred
-handleExiWait:
-addi r9, r9, 1
-cmpwi r9, 15
-bge- handleExiRetry
-lwz r10, 0x680C(r11)
-andi. r10, r10, 1
-bne handleExiWait
-
-#read values from transfer register to r3 for output
-lwz r3, 0x6810(r11) #read from transfer register
-
+#restore registers and sp
+lwz r0, 0x24(r1)
+addi r1, r1, 0x20
+mtlr r0
 blr
 
 ################################################################################
@@ -216,8 +209,8 @@ blr
 #  description: stops port B writes
 ################################################################################
 endExiTransfer:
-li r10, 0
-stw r10, 0x6800(r11) #write 0 to the parameter register
+//li r10, 0
+//stw r10, 0x6814(r11) #write 0 to the parameter register
 
 blr
 
